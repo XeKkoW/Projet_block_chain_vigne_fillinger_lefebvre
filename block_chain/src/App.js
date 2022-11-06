42aaa58f454d35a036289afd886ef0ff7967a333
XeKkoW <gabriel.vigne@edu.ece.fr>
2022-11-06 18:29:59 +0100

    visualisation compte balance

--- block_chain/src/App.js
+++ block_chain/src/App.js
@@ -4,15 +4,21 @@ import abi from './contracts/ABI.json';
 import { ethers } from 'ethers';
 import Home from './Home';
 import ContractAddress from './Contract';
+import Web3Modal from 'web3modal';
 
 //0xd1954846d3b257fed1f2dbb92da06339683615ad
-//0x45ca1bE138Bb5c2771697D07411ae30000852f9a
+//0x45ca1bE138Bb5c2771697D07411ae30000852f9a -- gab
 //0xA05FA9C6200096f75736D8feE762C53F597A4C67 -- Alex
 
 function App() {
 
   const [currentAccount, setCurrentAccount] = useState();
+  const [connect, setConnect] = useState(false);
+
   const [contractAddress, setContractAddress] = useState("" );
+  const { ethereum } = window;
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const [balance, setBalance] = useState()
 
 
   const checkWalletIsConnected = async () => {
@@ -34,6 +40,11 @@ function App() {
     } else {
       console.log("No authorized account found");
     }
+
+    const balance2 = await provider.getBalance(contractAddress);
+    const showBalance = `${ethers.utils.formatEther(balance2)}ETH\n`;
+    console.log(showBalance);
+    setBalance(showBalance);
   }
 
   const connectWalletHandler = async () => {
@@ -47,6 +58,7 @@ function App() {
       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
       console.log("Found an account! Address: ", accounts[0]);
       setCurrentAccount(accounts[0]);
+      window.location.reload();
     } catch (err) {
       console.log(err)
     }
@@ -54,10 +66,10 @@ function App() {
 
   const mintNftHandler = async () => {
     try {
-      const { ethereum } = window;
+      
 
       if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
+        
         const signer = provider.getSigner();
         const nftContract = new ethers.Contract(contractAddress, abi, signer);
 
@@ -115,10 +127,25 @@ function App() {
         <h1>TP ECE</h1>
         <ContractAddress newContract = {NewContract}/>
         {console.log("test", contractAddress)}
+        
+
+{!contractAddress ? "" :         
+<div>
+       <ul>
+          <li>Account</li>
+          <li>{contractAddress}</li>
+          <li>Balance</li>
+          <li>{balance}</li>
+        </ul>
+          </div>
+        }
+
+      
 
         <div>
           {currentAccount ? mintNftButton() : connectWalletButton()}
         </div>
+
       
       </div>
     </div>
